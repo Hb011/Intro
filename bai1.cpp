19eab9abe659781c23d7d5fbe49ac0049662ada4
Hb011 <bqhuyy.01@gmail.com>
2023-12-25 13:24:25 +0700

    ok

---
+++ bai1.cpp
@@ -0,0 +1,33 @@
+//cau1
+write a complete program meeting the following requirements
+a.input 1 positive integer n and K so that 0<n<1000; if the input value is not compliant to the rule, ask the user to input again until the rule is satisfied
+b.print out the sum S=11+22+33+....+nn 
+ example: N=4 -> S=11+22+33+44=1000
+          N=12 -> S=11+22+33+1010+1111+1212=3828
+
+
+//cau2
+write a complete program meeting the following requirements
+a. input an integer array with N elements, the output that array onto the console screen
+b. find and print out the element that contains most digits with 0 value. In case there are more than one such elements, print out the last one
+  example: 200 35 80 22 1010 7   ->   1010 (2zero digits)
+c.sort the array so that prime elements are in descending order, while other elements remain unchanged. Then, print out the resulting array 
+   example:  5 8 3 9 6 7 1
+        ->   7 8 5 9 6 3 1
+
+//cau3
+write a complete program meeting the following requirements
+a. declare and input values for a NxM matrix which contains integers, then output it onto the console screen
+b. check if the input matrix is an "X" matrix or not, with the definition tha an S matrix is a square matrix in which all the elements on 2 diagonas are non-zero and all the other elements must be zeroes
+  example: -3  0  0  0  3
+           0  5  0  -2  0
+           0  0  4  0  0
+           2  0  0  0  6
+           output: yes
+
+//cau4
+write a complete program meeting the following requirements
+a.declare and input value for a string containing no more than 200 characters
+calculate and print out the greatest NON-NEGATIVE NUMBER that appears in the string
+  example: input: Class 12A1 school year 2022-2023
+           output: 2023
